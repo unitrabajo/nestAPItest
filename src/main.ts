d9dc9805ab86bf9f28d52275518803e27c43b65c
MAriana Olivas <olivasgarcia031096@gmail.com>
2023-01-23 20:31:51 -0500

    env

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,15 @@ async function bootstrap() {
 
   app.enableCors();
 
-  await app.listen(3200);
+  await app.listen(48001 , '162.241.156.14', () => {
+
+      console.log('DB_PASSWORD : ', process.env.DB_PASSWORD);
+      console.log('DB_NAME : ', process.env.DB_NAME);
+      console.log('DB_HOST : ', process.env.DB_HOST);
+      console.log('DB_PORT : ', process.env.DB_PORT);
+      console.log('DB_USERNAME : ', process.env.DB_USERNAME);
+
+  });
 
 
 
